3c00594a40cd9dd6698e599b09fea0cf13bfb64c
Michal Urbanski <michalurbanski94@gmail.com>
2016-04-28 20:32:54 +0200

    Widok tworzenia rezerwacji

--- code/assets/js/eventReservations.js
+++ code/assets/js/eventReservations.js
@@ -1,12 +1,4 @@
 //DATA
-//event describing object
-eventObject = {
-  name: 'Romeo i Julia',
-  date: '23-05-2016',
-  hour: '17:00',
-  room: '4',
-  building: 'ul.Jagiellońska 12'
-}
 
 //reservations is an array of reservation objects
 reservations = [
@@ -40,19 +32,6 @@ reservations = [
 ]
 
 //LOGIC
-
-function fillEventDescriptionTable() {
-  var element = document.getElementById('eventName');
-  element.innerHTML = eventObject.name;
-  element = document.getElementById('eventDate');
-  element.innerHTML = eventObject.date;
-  element = document.getElementById('eventHour');
-  element.innerHTML = eventObject.hour;
-  element = document.getElementById('eventRoom');
-  element.innerHTML = eventObject.room;
-  element = document.getElementById('eventBuilding');
-  element.innerHTML = eventObject.building;
-}
 
 function fillEventReservationTable() {
   var tbody = document.getElementById('reservationsTableBody');
--- code/assets/js/general.js
+++ code/assets/js/general.js
@@ -1,5 +1,12 @@
 //DATA
-
+//event describing object
+eventObject = {
+  name: 'Romeo i Julia',
+  date: '23-05-2016',
+  hour: '17:00',
+  room: '4',
+  building: 'ul.Jagiellońska 12'
+}
 
 //LOGIC
 
@@ -7,3 +14,16 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+function fillEventDescriptionTable() {
+  var element = document.getElementById('eventName');
+  element.innerHTML = eventObject.name;
+  element = document.getElementById('eventDate');
+  element.innerHTML = eventObject.date;
+  element = document.getElementById('eventHour');
+  element.innerHTML = eventObject.hour;
+  element = document.getElementById('eventRoom');
+  element.innerHTML = eventObject.room;
+  element = document.getElementById('eventBuilding');
+  element.innerHTML = eventObject.building;
+}
