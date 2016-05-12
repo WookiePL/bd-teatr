1cda9a197f9feb7329352c0f050f70673b54d349
Tukan <michal.tukaj.94@gmail.com>
2016-05-12 18:55:41 +0200

    UsersScreen prawie gotowy. Zmiana sposobu edycji i dodawania usera (nowy screen okazał się zbędny). Do zrobienia: możliwość edycji praw na tym screenie, a nie osobnym.

--- AdminPanel/src/main/java/bd2/adminPanel/controllers/UsersController.java
+++ AdminPanel/src/main/java/bd2/adminPanel/controllers/UsersController.java
@@ -14,7 +14,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.BlurType;
@@ -53,29 +57,35 @@ public class UsersController implements Initializable {
 
     @FXML
     private TextField textFieldPhone;
-    
+
     @FXML
     private Button buttonAddUser;
-    
+
     @FXML
     private Button buttonEditUser;
-    
+
     @FXML
     private Button buttonEditRights;
-    
+
     @FXML
     private Button buttonBackMenu;
-    
+
     @FXML
     private Button buttonDeleteUser;
-    
+
     private final DropShadow shadow = new DropShadow(BlurType.THREE_PASS_BOX, Color.rgb(0, 0, 0, 0.4), 5, 0.0, 0, 1);
-    
-    private String greenButtonReleasedStyle;
-    
-    private final String greenButtonPressedStyle = "-fx-background-color: linear-gradient(#a9ff00, #f0ff35), radial-gradient(center 50% +140%, radius 200%, #b8ee36 45%, #80c800 50%);";
-    
-    private final String greenButtonOnMouseStyle = "-fx-background-color: linear-gradient(#f4ff57, #caff38), radial-gradient(center 50% -40%, radius 200%, #ceff52 45%, #92e600 50%);";
+
+    private final String greenButtonReleasedStyle = "-fx-background-color: linear-gradient(#f0ff35, #a9ff00), radial-gradient(center 50% -40%, radius 200%, #b8ee36 45%, #80c800 50%); -fx-background-radius: 6, 5; -fx-background-insets: 0, 1; -fx-text-fill: #395306;";
+
+    private final String greenButtonPressedStyle = "-fx-background-color: linear-gradient(#a9ff00, #f0ff35), radial-gradient(center 50% +140%, radius 200%, #b8ee36 45%, #80c800 50%); -fx-background-radius: 6, 5; -fx-background-insets: 0, 1; -fx-text-fill: #395306";
+
+    private final String greenButtonOnMouseStyle = "-fx-background-color: linear-gradient(#f4ff57, #caff38), radial-gradient(center 50% -40%, radius 200%, #ceff52 45%, #92e600 50%); -fx-background-radius: 6, 5; -fx-background-insets: 0, 1; -fx-text-fill: #395306";
+
+    private final String redButtonReleasedStyle = "-fx-background-color: linear-gradient(#ff3535, #ff0000), radial-gradient(center 50% -40%, radius 200%, #ee3636 45%, #c80000 50%); -fx-background-radius: 6, 5; -fx-background-insets: 0, 1; -fx-text-fill: #ffe3e3";
+
+    private final String redButtonPressedStyle = "-fx-background-color: linear-gradient(#ff0000, #ff3535), radial-gradient(center 50% +140%, radius 200%, #ee3636 45%, #c80000 50%); -fx-background-radius: 6, 5; -fx-background-insets: 0, 1; -fx-text-fill: #ffe3e3";
+
+    private final String redButtonOnMouseStyle = "-fx-background-color: linear-gradient(#ff7373, #ffa6a6), radial-gradient(center 50% -40%, radius 200%, #ff6b6b 45%, #f00000 50%); -fx-background-radius: 6, 5; -fx-background-insets: 0, 1; -fx-text-fill: #ffe3e3";
 
     private static User selectedUser;
 
@@ -98,27 +108,115 @@ public class UsersController implements Initializable {
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
-    
+
     @FXML
-    public void onMouseEntered() {
+    public void buttonAddUserOnMouseEntered() {
         buttonAddUser.setEffect(shadow);
         buttonAddUser.setStyle(greenButtonOnMouseStyle);
     }
-    
+
     @FXML
-    public void onMouseExited() {
+    public void buttonAddUserOnMouseExited() {
         buttonAddUser.setEffect(null);
         buttonAddUser.setStyle(greenButtonReleasedStyle);
     }
-    
+
     @FXML
-    public void onMousePressed() {
+    public void buttonAddUserOnMousePressed() {
         buttonAddUser.setStyle(greenButtonPressedStyle);
+    }
+
+    @FXML
+    public void buttonAddUserOnMouseReleased() {
+        buttonAddUser.setStyle(greenButtonReleasedStyle);
     }
     
     @FXML
-    public void onMouseReleased() {
-        buttonAddUser.setStyle(greenButtonReleasedStyle);
+    public void buttonEditUserOnMouseEntered() {
+        buttonEditUser.setEffect(shadow);
+        buttonEditUser.setStyle(greenButtonOnMouseStyle);
+    }
+
+    @FXML
+    public void buttonEditUserOnMouseExited() {
+        buttonEditUser.setEffect(null);
+        buttonEditUser.setStyle(greenButtonReleasedStyle);
+    }
+
+    @FXML
+    public void buttonEditUserOnMousePressed() {
+        buttonEditUser.setStyle(greenButtonPressedStyle);
+    }
+
+    @FXML
+    public void buttonEditUserOnMouseReleased() {
+        buttonEditUser.setStyle(greenButtonReleasedStyle);
+    }
+    
+    @FXML
+    public void buttonEditRightsOnMouseEntered() {
+        buttonEditRights.setEffect(shadow);
+        buttonEditRights.setStyle(greenButtonOnMouseStyle);
+    }
+
+    @FXML
+    public void buttonEditRightsOnMouseExited() {
+        buttonEditRights.setEffect(null);
+        buttonEditRights.setStyle(greenButtonReleasedStyle);
+    }
+
+    @FXML
+    public void buttonEditRightsOnMousePressed() {
+        buttonEditRights.setStyle(greenButtonPressedStyle);
+    }
+
+    @FXML
+    public void buttonEditRightsOnMouseReleased() {
+        buttonEditRights.setStyle(greenButtonReleasedStyle);
+    }
+    
+    @FXML
+    public void buttonBackMenuOnMouseEntered() {
+        buttonBackMenu.setEffect(shadow);
+        buttonBackMenu.setStyle(greenButtonOnMouseStyle);
+    }
+
+    @FXML
+    public void buttonBackMenuOnMouseExited() {
+        buttonBackMenu.setEffect(null);
+        buttonBackMenu.setStyle(greenButtonReleasedStyle);
+    }
+
+    @FXML
+    public void buttonBackMenuOnMousePressed() {
+        buttonBackMenu.setStyle(greenButtonPressedStyle);
+    }
+
+    @FXML
+    public void buttonBackMenuOnMouseReleased() {
+        buttonBackMenu.setStyle(greenButtonReleasedStyle);
+    }
+
+    @FXML
+    public void buttonDeleteUserOnMouseEntered() {
+        buttonDeleteUser.setEffect(shadow);
+        buttonDeleteUser.setStyle(redButtonOnMouseStyle);
+    }
+
+    @FXML
+    public void buttonDeleteUserOnMouseExited() {
+        buttonDeleteUser.setEffect(null);
+        buttonDeleteUser.setStyle(redButtonReleasedStyle);
+    }
+
+    @FXML
+    public void buttonDeleteUserOnMousePressed() {
+        buttonDeleteUser.setStyle(redButtonPressedStyle);
+    }
+
+    @FXML
+    public void buttonDeleteUserOnMouseReleased() {
+        buttonDeleteUser.setStyle(redButtonReleasedStyle);
     }
 
     @FXML
@@ -131,28 +229,29 @@ public class UsersController implements Initializable {
 
         User user = listViewUsers.getSelectionModel().getSelectedItem();
 
-        if (user == null) {
-            return;
-        }
+        if (user != null) {
+            textFieldId.setText(user.getId());
+            textFieldFirstName.setText(user.getFirstName());
+            textFieldLastName.setText(user.getLastName());
+            textFieldEmail.setText(user.getEmail());
+            textFieldPhone.setText(user.getPhone());
 
-        textFieldId.setText(user.getId());
-        textFieldFirstName.setText(user.getFirstName());
-        textFieldLastName.setText(user.getLastName());
-        textFieldEmail.setText(user.getEmail());
-        textFieldPhone.setText(user.getPhone());
+            buttonEditUser.setVisible(true);
+            buttonDeleteUser.setVisible(true);
 
-        if (user.getRights() != null) {
-            ObservableList<Right> rights = FXCollections.observableArrayList();
-            rights.addAll(user.getRights());
+            if (user.getRights() != null) {
+                ObservableList<Right> rights = FXCollections.observableArrayList();
+                rights.addAll(user.getRights());
 
-            listViewRights.setItems(rights);
-        } else {
-            listViewRights.setItems(null);
+                listViewRights.setItems(rights);
+            } else {
+                listViewRights.setItems(null);
+            }
         }
     }
 
     @FXML
-    public void addUser() {
+    public void addUser() {/*
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/AddUserScreen.fxml"));
         Pane pane = null;
 
@@ -165,38 +264,71 @@ public class UsersController implements Initializable {
         AddUserController addUserController = loader.getController();
         addUserController.setMainController(mainController);
 
-        mainController.setScreen(pane);
+        mainController.setScreen(pane);*/ // MO¯NA WYWALIÆ OKNO DODAWANIA USERA I KLASÊ KONTROLERA ZA NIE ODPOWIEDZIALN¥.
+
+        User user = new User(textFieldId.getText(), textFieldFirstName.getText(), textFieldLastName.getText(),
+                textFieldEmail.getText(), textFieldPhone.getText(), null);
+
+        userRepository.getUsers().add(user);
+        initialize(null, null);
     }
 
     @FXML
     public void deleteUser() {
         User user = listViewUsers.getSelectionModel().getSelectedItem();
-        userRepository.getUsers().remove(user);
 
-        initialize(null, null);
+        if (user != null) {
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.getButtonTypes().set(0, new ButtonType("Tak", ButtonBar.ButtonData.YES));
+            alert.getButtonTypes().set(1, new ButtonType("Nie", ButtonBar.ButtonData.NO));
+            alert.setTitle("Usun¹æ?");
+            alert.setHeaderText("Czy na pewno chcesz usun¹æ poni¿szego u¿ytkownika?");
+            alert.setContentText("Login: " + user.getId() + ", Imiê i nazwisko: " + user.getFirstName() + " " + user.getLastName());
+
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs.getButtonData() == ButtonBar.ButtonData.YES) {
+                    userRepository.getUsers().remove(user);
+                    initialize(null, null);
+                }
+            });
+        }
     }
 
     @FXML
     public void editUser() {
-        if (listViewUsers.getSelectionModel().getSelectedItem() == null) {
-            return;
+        User user = listViewUsers.getSelectionModel().getSelectedItem();
+        if (user != null) {
+            selectedUser = user;
+            /*
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/EditUserScreen.fxml"));
+            Pane pane = null;
+
+            try {
+                pane = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            EditUserController editUserController = loader.getController();
+            editUserController.setMainController(mainController);
+
+            mainController.setScreen(pane);
+             */ // MO¯NA WYWALIÆ OKNO EDYTOWANIA USERA I KLASÊ KONTROLERA ZA NIE ODPOWIEDZIALN¥.
+
+            user.setId(textFieldId.getText());
+            user.setFirstName(textFieldFirstName.getText());
+            user.setLastName(textFieldLastName.getText());
+            user.setEmail(textFieldEmail.getText());
+            user.setPhone(textFieldPhone.getText());
+
+            for (int index = 0; index < userRepository.getUsers().size(); ++index) {
+                if (userRepository.getUsers().get(index).getId().equals(selectedUser.getId())) {
+                    userRepository.getUsers().set(index, user);
+                }
+            }
+
+            initialize(null, null);
         }
-
-        selectedUser = listViewUsers.getSelectionModel().getSelectedItem();
-
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/EditUserScreen.fxml"));
-        Pane pane = null;
-
-        try {
-            pane = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        EditUserController editUserController = loader.getController();
-        editUserController.setMainController(mainController);
-
-        mainController.setScreen(pane);
     }
 
     @FXML
@@ -227,9 +359,11 @@ public class UsersController implements Initializable {
         ObservableList<User> users = FXCollections.observableArrayList();
         users.addAll(userRepository.getUsers());
 
-        greenButtonReleasedStyle = buttonAddUser.getStyle();
-        
+        buttonEditUser.setVisible(false);
+        buttonDeleteUser.setVisible(false);
+
         listViewUsers.setItems(users);
+        listViewUsers.refresh();
     }
 
 }
--- AdminPanel/src/main/java/bd2/adminPanel/tmp/User.java
+++ AdminPanel/src/main/java/bd2/adminPanel/tmp/User.java
@@ -3,73 +3,83 @@ package bd2.adminPanel.tmp;
 import java.util.List;
 
 public class User {
-	private String id;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String phone;
-	private List<Right> rights;
 
-	public String getId() {
-		return id;
-	}
+    private String id;
+    private String login;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private List<Right> rights;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public String getLogin() {
+        return login;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public void setLogin(String id) {
+        this.login = login;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public List<Right> getRights() {
-		return rights;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setRights(List<Right> rights) {
-		this.rights = rights;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public User(String id, String firstName, String lastName, String email, String phone, List<Right> rights) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.phone = phone;
-		this.rights = rights;
-	}
+    public String getPhone() {
+        return phone;
+    }
 
-	@Override
-	public String toString() {
-		return "Id: " + id + ", "+ firstName + " " + lastName;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Right> getRights() {
+        return rights;
+    }
+
+    public void setRights(List<Right> rights) {
+        this.rights = rights;
+    }
+
+    public User(String id, String firstName, String lastName, String email, String phone, List<Right> rights) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.rights = rights;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + ", " + firstName + " " + lastName;
+    }
 }
