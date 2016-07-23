b9c07aaf445129a89035936e386ed67f4da4f48d
Mateusz Szczesny <Mateusz Szczesny>
2016-07-23 18:59:30 +0200

    Drobne porządki - usunięcie zbędnych fragmentów kodu powstałych w trakcie walki ze zrozumieniem aplikacji

--- BDII/src/main/java/theater/controller/EventController.java
+++ BDII/src/main/java/theater/controller/EventController.java
@@ -45,13 +45,6 @@ public class EventController {
     }
 
     @PreAuthorize("hasRole('ROLE_CASHIER')")
-    @RequestMapping(value = {"/priceList"}, method = RequestMethod.GET)
-    public String priceList(Model model) {
-        model.addAttribute("priceList", eventService.getAllEventRealization());
-        return "priceList";
-    }
-
-    @PreAuthorize("hasRole('ROLE_CASHIER')")
     @RequestMapping(value = {"/events"}, method = RequestMethod.GET)
     public String events(Model model) {
         model.addAttribute("events", eventService.getAllEvents());
