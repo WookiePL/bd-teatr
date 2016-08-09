31a2979dac01f6f5886cd2ca4bbfc522f9624e76
Mateusz Szczesny <lifford@orange.pl>
2016-08-09 12:23:30 +0200

    JavaDoc dla EventService

--- BDII/src/main/java/theater/services/EventService.java
+++ BDII/src/main/java/theater/services/EventService.java
@@ -53,7 +53,11 @@ public class EventService implements IEventService {
     @Autowired
     private ModelMapper modelMapper;
 
-
+    /**
+     * Returns complete list of places.
+     *
+     * @return List<PlaceDTO>
+     */
     @Override
     public List<PlaceDTO> getAllPlaces(){
         List<PlaceDTO> placeDTOs = new ArrayList<>();
@@ -63,6 +67,11 @@ public class EventService implements IEventService {
         return placeDTOs;
     }
 
+    /**
+     * Returns complete list of event ralizations.
+     *
+     * @return List<EventRealizationDTO>
+     */
     @Override
     public List<EventRealizationDTO> getAllEventRealization() {
         List<EventRealizationDTO> realizationDTOs = new ArrayList<>();
@@ -72,6 +81,11 @@ public class EventService implements IEventService {
         return realizationDTOs;
     }
 
+    /**
+     * Returns complete list of events.
+     *
+     * @return List<EventDTO>
+     */
     public List<EventDTO> getAllEvents() {
         List<EventDTO> eventDTOs = new ArrayList<>();
         for(EventEntity list: eventDAO.getAll()){
@@ -80,6 +94,13 @@ public class EventService implements IEventService {
         return eventDTOs;
     }
 
+    /**
+     * Adding event with given parms.
+     *
+     * @param eventName
+     * @param eventTypeId
+     * @param eventDescription
+     */
     @Override
     public void addEvent(String eventName, Integer eventTypeId, String eventDescription){
         EventDTO event = new EventDTO();
@@ -89,6 +110,11 @@ public class EventService implements IEventService {
         eventDAO.addEvent(convertToEntity(event));
     }
 
+    /**
+     * Returms all event types
+     *
+     * @return List<EventTypeDTO>
+     */
     public List<EventTypeDTO> getAllEventTypes() {
         List<EventTypeDTO> eventTypesDTOs = new ArrayList<>();
         for(EventTypeEntity list: eventTypeDAO.getAll()){
@@ -97,16 +123,35 @@ public class EventService implements IEventService {
         return eventTypesDTOs;
     }
 
+    /**
+     * Returns event with given id.
+     *
+     * @param id
+     * @return EventDTO
+     */
     @Override
     public EventDTO getEventById(Integer id) {
         return convertToDto(eventDAO.getEventById(id));
     }
 
+    /**
+     * Deletes event with given id.
+     *
+     * @param id
+     */
     @Override
     public void deleteEvent(Integer id) {
         eventDAO.deleteEvent(id);
     }
 
+    /**
+     * Update event according to parms.
+     *
+     * @param eventId
+     * @param eventName
+     * @param eventType
+     * @param eventDescription
+     */
     @Override
     public void updateEvent(Integer eventId, String eventName, Integer eventType, String eventDescription) {
         EventDTO event = convertToDto(eventDAO.getEventById(eventId));
@@ -116,6 +161,12 @@ public class EventService implements IEventService {
         eventDAO.updateEvent(convertToEntity(event));
     }
 
+    /**
+     * Returns all reservations of realization identified by id.
+     *
+     * @param id
+     * @return List<ReservationDTO>
+     */
     @Override
     public List<ReservationDTO> getAllEventReservationByRealization(int id) {
         List<ReservationDTO> reservationDTOs = new ArrayList<>();
@@ -125,6 +176,11 @@ public class EventService implements IEventService {
         return reservationDTOs;
     }
 
+    /**
+     * Returns all event reservations.
+     *
+     * @return List<ReservationDTO>
+     */
     @Override
     public List<ReservationDTO> getAllEventReservation() {
         List<ReservationDTO> reservationDTOs = new ArrayList<>();
@@ -134,6 +190,11 @@ public class EventService implements IEventService {
         return reservationDTOs;
     }
 
+    /**
+     * Returns all buildings.
+     *
+     * @return List<BuildingDTO>
+     */
     @Override
     public List<BuildingDTO> getAllBuildings() {
         List<BuildingDTO> buildingDTOs = new ArrayList<>();
@@ -143,6 +204,11 @@ public class EventService implements IEventService {
         return buildingDTOs;
     }
 
+    /**
+     * Returns all rooms.
+     *
+     * @return List<RoomDTO>
+     */
     @Override
     public List<RoomDTO> getAllRooms() {
         List<RoomDTO> roomDTOs = new ArrayList<>();
@@ -152,6 +218,14 @@ public class EventService implements IEventService {
         return roomDTOs;
     }
 
+    /**
+     * Add realization described by parms.
+     *
+     * @param eventId
+     * @param roomId
+     * @param realizationDate
+     * @param realizationTime
+     */
     @Override
     public void addRealization(Integer eventId, Integer roomId, java.sql.Date realizationDate, Integer realizationTime){
         EventRealizationDTO eventRealization = new EventRealizationDTO();
@@ -162,6 +236,15 @@ public class EventService implements IEventService {
         eventRealizationDAO.addEventRealization(convertToEntity(eventRealization));
     }
 
+    /**
+     * Update reservation described by parms.
+     *
+     * @param realizationId
+     * @param eventId
+     * @param roomId
+     * @param realizationDate
+     * @param realizationHour
+     */
     @Override
     public void updateRealization(Integer realizationId, Integer eventId, Integer roomId, java.sql.Date realizationDate, Integer realizationHour) {
         EventRealizationDTO eventRealization = convertToDto(eventRealizationDAO.getEventRealizationById(realizationId));
@@ -172,11 +255,18 @@ public class EventService implements IEventService {
         eventRealizationDAO.updateEventRealization(convertToEntity(eventRealization));
     }
 
+    //TODO dlaczemu null?
     @Override
     public List<SectorDTO> getAllSectorsByRealizationId(int id) {
         return null;
     }
 
+    /**
+     * Get all reservations of event identified by id.
+     *
+     * @param id
+     * @return List<ReservationDTO>
+     */
     @Override
     public List<ReservationDTO> getAllEventReservation(Integer id) {
         List<ReservationDTO> reservationDTOs = new ArrayList<>();
@@ -186,17 +276,34 @@ public class EventService implements IEventService {
         return reservationDTOs;
     }
 
-
+    /**
+     * Returns reservation with given id.
+     *
+     * @param id
+     * @return ReservationDTO
+     */
     @Override
     public ReservationDTO getReservationById(Integer id) {
         return convertToDto(reservationDAO.getReservationById(id));
     }
 
+    /**
+     * Returns event realization with given id.
+     *
+     * @param id
+     * @return EventRealizationDTO
+     */
     @Override
     public EventRealizationDTO getEventRealizationById(Integer id) {
         return convertToDto(eventRealizationDAO.getEventRealizationById(id));
     }
 
+    /**
+     * Returns information of seats in room where realization is set.
+     *
+     * @param realizationId
+     * @return List<SectorInfo>
+     */
     @Override
     public List<SectorInfo> getRoomInfo(Integer realizationId) {
         EventRealizationDTO event = convertToDto(eventRealizationDAO.getEventRealizationById(realizationId));
@@ -233,6 +340,12 @@ public class EventService implements IEventService {
         return roomInfo;
     }
 
+    /**
+     * Returns converted list of seats.
+     *
+     * @param seats
+     * @return List<PlaceDTO>
+     */
     @Override
     public List<PlaceDTO> convertSelectedSeatsStringArrayToPlaceList(String[] seats) {
         List<PlaceDTO> places = new ArrayList<>();
@@ -255,6 +368,12 @@ public class EventService implements IEventService {
         return places;
     }
 
+    /**
+     * Returns selecrteted seats converted to entity obiects.
+     *
+     * @param seats
+     * @return List<PlaceEntity>
+     */
     @Override
     public List<PlaceEntity> convertSelectedSeatsStringArrayToPlaceEntityList(String[] seats) {
         List<PlaceEntity> places = new ArrayList<>();
@@ -277,6 +396,12 @@ public class EventService implements IEventService {
         return places;
     }
 
+    /**
+     * Returns list of places from list of ids.
+     *
+     * @param placeIds
+     * @return List<PlaceDTO>
+     */
     @Override
     public List<PlaceDTO> getPlaceListFromIds(String[] placeIds) {
         List<PlaceDTO> placeList = new ArrayList<>();
@@ -286,6 +411,13 @@ public class EventService implements IEventService {
         return placeList;
     }
 
+    /**
+     * Add reservation described by parms.
+     *
+     * @param reservation
+     * @param eventRealization
+     * @param selectedPlaces
+     */
     @Override
     public void addReservation(ReservationDTO reservation, EventRealizationDTO eventRealization, List<PlaceDTO> selectedPlaces){
         reservation.setEventRealizationId(eventRealization.getEventRealizationId());
@@ -295,6 +427,12 @@ public class EventService implements IEventService {
         reservationDAO.addReservation(convertToEntity(reservation));
     }
 
+    /**
+     * Add ticket described by parsm.
+     *
+     * @param eventRealization
+     * @param selectedPlaces
+     */
     @Override
     public void addTicket(EventRealizationDTO eventRealization, List<PlaceDTO> selectedPlaces) {
         TicketEntity ticket = new TicketEntity();
@@ -305,6 +443,12 @@ public class EventService implements IEventService {
         ticketDAO.addTicket(ticket);
     }
 
+    /**
+     * Update reservation identified by id according to parms.
+     *
+     * @param reservationId
+     * @param reservation
+     */
     @Override
     public void updateReservation(Integer reservationId, ReservationDTO reservation) {
         ReservationDTO dto = convertToDto(reservationDAO.getReservationById(reservationId));
@@ -314,11 +458,23 @@ public class EventService implements IEventService {
         reservationDAO.updateReservation(convertToEntity(reservation));
     }
 
+    /**
+     * Delete event realization identified by id.
+     *
+     * @param id
+     */
     @Override
     public void deleteEventRealization(Integer id) {
         eventRealizationDAO.deleteEventRealization(id);
     }
 
+    /**
+     * Returns price list to event identified by id and in given date.
+     *
+     * @param eventId
+     * @param date
+     * @return PriceListEntity
+     */
     @Override
     public PriceListEntity getPriceListForEvent(Integer eventId, Date date) {
         return priceListDAO.findByEventIdAndDate(eventId, date);
@@ -330,11 +486,21 @@ public class EventService implements IEventService {
 //        return null;
     }
 
+    /**
+     * Delete reservation identified by id.
+     *
+     * @param id
+     */
     @Override
     public void deleteReservation(Integer id) {
         reservationDAO.deleteReservation(id);
     }
 
+    /**
+     * Returns all price lists.
+     *
+     * @return List<PriceListDTO>
+     */
     @Override
     public List<PriceListDTO> getAllPriceList() {
         List<PriceListDTO> priceListDTOs = new ArrayList<>();
@@ -344,16 +510,33 @@ public class EventService implements IEventService {
         return priceListDTOs;
     }
 
+    /**
+     * Returns price list identified by id.
+     *
+     * @param id
+     * @return PriceListDTO
+     */
     @Override
     public PriceListDTO getPriceListById(Integer id) {
         return convertToDto(priceListDAO.getPriceListById(id));
     }
 
+    /**
+     * Delete price list identified by id.
+     *
+     * @param id
+     */
     @Override
     public void deletePriceList(Integer id) {
         priceListDAO.deletePriceList(id);
     }
 
+    /**
+     * Update price list identified by id according to parms.
+     *
+     * @param priceListID
+     * @param priceList
+     */
     @Override
     public void updatePriceList(Integer priceListID, PriceListDTO priceList) {
         PriceListDTO dto = convertToDto(priceListDAO.getPriceListById(priceListID));
@@ -364,14 +547,34 @@ public class EventService implements IEventService {
         priceListDAO.updatePriceList(convertToEntity(priceList));
     }
 
+    /**
+     * Convert Entity object to DTO object
+     * @param buildingEntity
+     * @return BuildingDTO
+     */
     private BuildingDTO convertToDto(BuildingEntity buildingEntity) {
         return modelMapper.map(buildingEntity, BuildingDTO.class);
     }
 
+    /**
+     * Convert Entity object to DTO object
+     *
+     * @param roomEntity
+     * @return RoomDTO
+     */
     private RoomDTO convertToDto(RoomEntity roomEntity) {
         return modelMapper.map(roomEntity, RoomDTO.class);
     }
 
+    /**
+     * Update price list identified by id according to given parms.
+     *
+     * @param priceListId
+     * @param priceListFrom
+     * @param priceListTo
+     * @param priceListName
+     * @param eventId
+     */
     @Override
     public void updatePriceList(Integer priceListId, String priceListFrom, String priceListTo, String priceListName, Integer eventId) {
         PriceListDTO priceList = convertToDto(priceListDAO.getPriceListById(priceListId));
@@ -382,6 +585,14 @@ public class EventService implements IEventService {
         priceListDAO.updatePriceList(convertToEntity(priceList));
     }
 
+    /**
+     * Add price list according to given parms.
+     *
+     * @param priceListFrom
+     * @param priceListTo
+     * @param priceListName
+     * @param eventId
+     */
     @Override
     public void addPriceList(String priceListFrom, String priceListTo, String priceListName, Integer eventId) {
         PriceListDTO priceList = new PriceListDTO();
@@ -392,28 +603,62 @@ public class EventService implements IEventService {
         priceListDAO.addPriceList(convertToEntity(priceList));
     }
 
+    /**
+     * Convert Entity object to DTO object
+     *
+     * @param priceListEntity
+     * @return PriceListDTO
+     */
     private PriceListDTO convertToDto(PriceListEntity priceListEntity) {
         return modelMapper.map(priceListEntity, PriceListDTO.class);
     }
 
+    /**
+     * Convert Entity object to DTO object
+     * @param eventEntity
+     * @return EventDTO
+     */
     private EventDTO convertToDto(EventEntity eventEntity) {
         return modelMapper.map(eventEntity, EventDTO.class);
     }
 
+    /**
+     * Convert Entity object to DTO object
+     *
+     * @param eventTypeEntity
+     * @return EventTypeDTO
+     */
     private EventTypeDTO convertToDto(EventTypeEntity eventTypeEntity) {
         return modelMapper.map(eventTypeEntity, EventTypeDTO.class);
     }
 
+    /**
+     * Convert Entity object to DTO object
+     *
+     * @param placeEntity
+     * @return PlaceDTO
+     */
     private PlaceDTO convertToDto(PlaceEntity placeEntity) {
         PlaceDTO place = modelMapper.map(placeEntity, PlaceDTO.class);
         return place;
     }
 
+    /**
+     * Convert Entity object to DTO object
+     * @param reservationEntity
+     * @return ReservationDTO
+     */
     private ReservationDTO convertToDto(ReservationEntity reservationEntity) {
         ReservationDTO reservationDTO = modelMapper.map(reservationEntity, ReservationDTO.class);
         return reservationDTO;
     }
 
+    /**
+     * Convert place list from list of dtos to list of entities.
+     *
+     * @param placeDTOList
+     * @return  List<PlaceEntity>
+     */
     @Override
     public List<PlaceEntity> convertToEntity(List<PlaceDTO> placeDTOList) {
         List<PlaceEntity> places = new ArrayList<>();
@@ -423,6 +668,12 @@ public class EventService implements IEventService {
         return places;
     }
 
+    /**
+     * Convert reservations from list of dtos to list of entities.
+     *
+     * @param reservationDTOs
+     * @return List<ReservationEntity>
+     */
     private List<ReservationEntity> convertToEntity1(List<ReservationDTO> reservationDTOs) {
         List<ReservationEntity> reservations = new ArrayList<>();
         for(ReservationDTO r: reservationDTOs){
@@ -431,34 +682,81 @@ public class EventService implements IEventService {
         return reservations;
     }
 
+    /**
+     * Convert dto obiect to entity object.
+     *
+     * @param ticketDTO
+     * @return TicketEntity
+     * @throws ParseException
+     */
     private TicketEntity convertToEntity(TicketDTO ticketDTO) throws ParseException {
         return modelMapper.map(ticketDTO, TicketEntity.class);
     }
 
+    /**
+     * Convert dto obiect to entity object.
+     *
+     * @param eventDTO
+     * @return EventEntity
+     * @throws ParseException
+     */
     private EventEntity convertToEntity(EventDTO eventDTO) throws ParseException {
         return modelMapper.map(eventDTO, EventEntity.class);
     }
 
+    /**
+     * Convert dto obiect to entity object.
+     *
+     * @param placeDTO
+     * @return PlaceEntity
+     * @throws ParseException
+     */
     private PlaceEntity convertToEntity(PlaceDTO placeDTO) throws ParseException {
         PlaceEntity placeEntity = modelMapper.map(placeDTO, PlaceEntity.class);
         return placeEntity;
     }
 
+    /**
+     * Convert dto obiect to entity object.
+     *
+     * @param reservationDTO
+     * @return ReservationEntity
+     * @throws ParseException
+     */
     private ReservationEntity convertToEntity(ReservationDTO reservationDTO) throws ParseException {
         ReservationEntity reservation = modelMapper.map(reservationDTO, ReservationEntity.class);
         return reservation;
     }
 
+    /**
+     * Convert dto obiect to entity object.
+     *
+     * @param priceListDTO
+     * @return PriceListEntity
+     * @throws ParseException
+     */
     private PriceListEntity convertToEntity(PriceListDTO priceListDTO) throws ParseException {
         PriceListEntity priceList = modelMapper.map(priceListDTO, PriceListEntity.class);
         return priceList;
     }
 
+    /**
+     * Convert Entity object to DTO object
+     *
+     * @param eventRealizationEntity
+     * @return EventRealizationDTO
+     */
     private EventRealizationDTO convertToDto(EventRealizationEntity eventRealizationEntity) {
         EventRealizationDTO postDto = modelMapper.map(eventRealizationEntity, EventRealizationDTO.class);
         return postDto;
     }
 
+    /**
+     * Convert dto obiect to entity object.
+     *
+     * @param eventRealization
+     * @return EventRealizationEntity
+     */
     @Override
     public EventRealizationEntity convertToEntity(EventRealizationDTO eventRealization) {
         EventRealizationEntity realizationEntity = modelMapper.map(eventRealization, EventRealizationEntity.class);
